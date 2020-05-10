b9cb00c751ea12c43999f5aeae2dcdd2bec60413
Kevin Li <kevinli5506@gmail.com>
2020-05-10 16:40:11 +0700

    News Detail no comment

---
+++ app/src/main/java/com/kevinli5506/appta/NewsDetailPage.kt
@@ -0,0 +1,12 @@
+package com.kevinli5506.appta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsDetailPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail_page)
+    }
+}
