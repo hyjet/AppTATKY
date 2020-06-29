50519a3d213fd3de42d5a5754ad7b2a395378b4b
Kevin Li <kevinli5506@gmail.com>
2020-06-29 22:10:28 +0700

    UI complete

---
+++ app/src/main/java/com/kevinli5506/appta/EventPage.kt
@@ -0,0 +1,12 @@
+package com.kevinli5506.appta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_page)
+    }
+}
